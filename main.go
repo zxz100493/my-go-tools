b44dabc8c9b2adc9fdae9d129a9506cd556767ca
zxz100493 <zxz10049@163.com>
2023-07-30 17:18:28 +0800

    fix goroutine num

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "my-go-tools/cmd"
+import (
+	"log"
+	"my-go-tools/cmd"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
 
 func main() {
+	//远程获取pprof数据
+	go func() {
+		log.Println(http.ListenAndServe("localhost:8080", nil))
+	}()
 	cmd.Execute()
+	time.Sleep(60 * time.Second)
 }
-
-
-
-
--- service/findFile.go
+++ service/findFile.go
@@ -35,7 +35,7 @@ func Find(cfg *Config) {
 	var tick <-chan time.Time
 
 	if cfg.Visible {
-		tick = time.Tick(100 * time.Millisecond)
+		tick = time.Tick(1000 * time.Millisecond)
 	}
 
 	go func() {
@@ -51,11 +51,17 @@ loop:
 		case size, ok := <-fileNum:
 			if !ok {
 				fmt.Println("not ok")
+				break loop
 			}
+			// fmt.Println("i amd select filenum")
 			num += size
 		case <-tick:
 			fmt.Println("\n num is:", num)
-		case s := <-filePath:
+		case s, ok := <-filePath:
+			if !ok {
+				// filePath channel is closed
+				break loop // exit the loop when the channel is closed
+			}
 			fmt.Printf("file path is:%s\n", s)
 			fmt.Println("break")
 			break loop
@@ -72,19 +78,28 @@ func readDir(file string, dir string, n *sync.WaitGroup) string {
 		if entry.IsDir() {
 			// fmt.Printf("it is dir need read continue:%s\n", filepath.Join(dir, entry.Name()))
 			n.Add(1)
-			go readDir(file, filepath.Join(dir, entry.Name()), n)
+			sema <- struct{}{}
+			subdir := filepath.Join(dir, entry.Name())
+			go func(subdir string) {
+				readDir(file, subdir, n)
+				<-sema
+			}(subdir)
 		} else {
+			fmt.Println("file does not have readable permission")
+
 			info, err := os.Stat(filepath.Join(dir, entry.Name()))
 			if err != nil {
 				// fmt.Printf("error getting file info: %v", err)
 				continue
 			}
-			if info.Mode().Perm()&0444 == 0444 {
-				// fmt.Println("file has readable permission")
-			} else {
-				continue
-				// fmt.Println("file does not have readable permission")
+			if !info.Mode().IsRegular() {
+				continue // 忽略非普通文件（如软链接）
 			}
+			if !hasReadPermission(info) {
+				// fmt.Println("file does not have readable permission")
+				continue
+			}
+			fmt.Println("send ", entry.Name())
 			fileNum <- 1
 
 			// fmt.Printf("entry name is:%s file name is:%s\n", entry.Name(), file)
@@ -97,13 +112,18 @@ func readDir(file string, dir string, n *sync.WaitGroup) string {
 			}
 		}
 	}
-	fmt.Printf("\ncan't find %s in target path %s\n", file, dir)
+	// fmt.Printf("\ncan't find %s in target path %s\n", file, dir)
 	return "nil"
+}
+
+func hasReadPermission(fi os.FileInfo) bool {
+	mode := fi.Mode()
+	return mode.IsRegular() || mode&os.ModeSymlink == os.ModeSymlink || mode&0400 == 0400
 }
 
 func dirents(dir string) []fs.DirEntry {
 
-	defer func() {
+	/* defer func() {
 		<-sema
 	}()
 
@@ -112,7 +132,7 @@ func dirents(dir string) []fs.DirEntry {
 	case <-done:
 		fmt.Println("----done------")
 		return nil
-	}
+	} */
 
 	entries, err := os.ReadDir(dir)
 	if err != nil {
